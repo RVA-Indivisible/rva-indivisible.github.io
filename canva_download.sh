e21c464d6166c0205ad6a7f35f4714e4d9280394
Gabe <gpacuilla@gmail.com>
2025-03-26 12:09:13 -0400

    initial commit

---
+++ canva_download.sh
@@ -0,0 +1,13 @@
+wget -E -k -p https://rvaindivisible.my.canva.site/
+#wget https://rvaindivisible.my.canva.site/_assets/media/979aae7099f79a779d536deee0dc8e23.jpg
+#wget https://rvaindivisible.my.canva.site/_assets/media/4dd7e490a8338830819a6636d93ef05f.svg
+#wget https://rvaindivisible.my.canva.site/_assets/media/bc225aa729502c9924a7e47bf838f78d.jpg
+#wget https://rvaindivisible.my.canva.site/_assets/media/2c39797aee7dcb9aaeb39bca2f9e1668.jpg
+#wget https://rvaindivisible.my.canva.site/_assets/media/e61c27aff32220fba65cfbbec4a1a818.jpg
+#wget https://rvaindivisible.my.canva.site/_assets/media/2c39797aee7dcb9aaeb39bca2f9e1668.jpg
+#wget https://rvaindivisible.my.canva.site/_assets/media/5b214baf5a97f9d7569fbd08de03dd31.jpg
+#wget https://rvaindivisible.my.canva.site/_assets/fonts/28079cb92e949f9dc7acfe61dfe2a0a9.woff2
+#wget https://rvaindivisible.my.canva.site/_assets/fonts/d6eb5721f46301170397393323ce8a86.woff2
+#wget https://rvaindivisible.my.canva.site/_assets/fonts/e3741b24826d89cccb250297aac9c43b.woff2
+#wget https://rvaindivisible.my.canva.site/_assets/fonts/ee82d41f23c8c5465af1fc940da4a36c.woff2
+#wget https://rvaindivisible.my.canva.site/_assets/fonts/fa0977e05da69370ebb53ec25bbfdd11.woff2
