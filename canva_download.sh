b976b5e1d05c68337ea4d90fffc65abfba7d1aa2
Gabe <gpacuilla@gmail.com>
2025-03-28 23:45:38 -0400

    style tweaks and add .nojekyll

--- canva_download.sh
+++ canva_download.sh
@@ -1,29 +1,26 @@
 # Would be nice if this worked:
-# chromium --headless --window-size=1920,1080 --run-all-compositor-stages-before-draw --virtual-time-budget=9000 --incognito --dump-dom https://rvaindivisible.my.canva.site/ | monolith - -I -b https://rvaindivisible.my.canva.site/ -o index.html
+# chromium --headless --window-size=1920,1080 --run-all-compositor-stages-before-draw --virtual-time-budget=9000 --incognito --dump-dom https://${CANVA_SRC_DOMAIN}/ | monolith - -I -b https://rvaindivisible.my.canva.site/ -o index.html
 
-#wget -E -k -p https://rvaindivisible.my.canva.site/
-wget -mpck --html-extension -e robots=off https://rvaindivisible.my.canva.site/
+CANVA_SRC_DOMAIN=rvaindivisible.my.canva.site
 
-mkdir rvaindivisible.my.canva.site/_assets/media/
-mkdir rvaindivisible.my.canva.site/_assets/fonts/
+wget -mpck --html-extension -e robots=off https://${CANVA_SRC_DOMAIN}/
 
-for media_asset in $(grep -o '_assets\/media\/\w\+\.\w\+' rvaindivisible.my.canva.site/index.html 2>/dev/null | sort -u); do
-    wget rvaindivisible.my.canva.site/${media_asset} -O rvaindivisible.my.canva.site/${media_asset}
+mkdir ${CANVA_SRC_DOMAIN}/_assets/media/
+mkdir ${CANVA_SRC_DOMAIN}/_assets/fonts/
+
+for media_asset in $(grep -o '_assets\/media\/\w\+\.\w\+' ${CANVA_SRC_DOMAIN}/index.html 2>/dev/null | sort -u); do
+    wget ${CANVA_SRC_DOMAIN}/${media_asset} -O ${CANVA_SRC_DOMAIN}/${media_asset}
 done
 
-for font_asset in $(grep -o '_assets\/fonts\/\w\+\.\w\+' rvaindivisible.my.canva.site/index.html 2>/dev/null | sort -u); do
-    wget rvaindivisible.my.canva.site/${font_asset} -O rvaindivisible.my.canva.site/${font_asset}
+for font_asset in $(grep -o '_assets\/fonts\/\w\+\.\w\+' ${CANVA_SRC_DOMAIN}/index.html 2>/dev/null | sort -u); do
+    wget ${CANVA_SRC_DOMAIN}/${font_asset} -O ${CANVA_SRC_DOMAIN}/${font_asset}
 done
-
-# GitHub pages didn't seem to like _assets
-sed -i -e 's/_assets/assets/g' rvaindivisible.my.canva.site/index.html
-mv rvaindivisible.my.canva.site/_assets rvaindivisible.my.canva.site/assets
 
 # Clean up existing content in root
-rm -fr index.html assets/
+rm -fr index.html _assets/
 
 # Move new content to root
-mv rvaindivisible.my.canva.site/* .
+mv ${CANVA_SRC_DOMAIN}/* .
 
 # Remove leftover staging directory
-rm -fr rvaindivisible.my.canva.site/
+rm -fr ${CANVA_SRC_DOMAIN}/
